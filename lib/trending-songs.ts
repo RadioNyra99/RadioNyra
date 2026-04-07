e6ae006fb451859c79c7f6cc2a58b13780c781ef
VadlapudiPranab <--get>
2026-04-07 16:23:43 +0530

    feat: updated trending songs and externalized to lib/trending-songs.ts

---
+++ lib/trending-songs.ts
@@ -0,0 +1,7 @@
+export const TRENDING_SONGS = [
+    { title: "O Maahi (Dunki)", artist: "Arijit Singh", hits: "4.2M" },
+    { title: "Pehle Bhi Main (Animal)", artist: "Vishal Mishra", hits: "3.8M" },
+    { title: "Kurchi Madathapetti (Guntur Kaaram)", artist: "Sri Krishna, Sahithi Chaganti", hits: "3.5M" },
+    { title: "Husn", artist: "Anuv Jain", hits: "2.9M" },
+    { title: "Sajni (Laapataa Ladies)", artist: "Arijit Singh", hits: "2.1M" },
+];

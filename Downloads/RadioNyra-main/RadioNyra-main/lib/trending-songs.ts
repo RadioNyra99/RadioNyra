d52c40fab0e3f34c8ee1c8b7167d88af218e4070
VadlapudiPranab <--get>
2026-04-20 22:55:48 +0530

    Update trending songs to the latest 2026 Bollywood hits

---
+++ Downloads/RadioNyra-main/RadioNyra-main/lib/trending-songs.ts
@@ -0,0 +1,7 @@
+export const TRENDING_SONGS = [
+    { title: "Tu Meri Main Tera", artist: "Vishal-Sheykhar", hits: "16.1M" },
+    { title: "Barbaad (Saiyaara)", artist: "Jubin Nautiyal", hits: "14.8M" },
+    { title: "Usey Kehna (Tere Ishk Mein)", artist: "A.R. Rahman & Jonita Gandhi", hits: "14.2M" },
+    { title: "CHILLGUM", artist: "Yo Yo Honey Singh & Ragini Vishwakarma", hits: "13.5M" },
+    { title: "Kya Bataun Tujhe (Pagalpan)", artist: "Vishal Mishra", hits: "12.0M" },
+];

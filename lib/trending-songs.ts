ff8eb8c0aa6ad27afc3143542476068d7e85dacf
Radio Nyra Admin <admin@radionyra.com>
2026-04-23 10:19:38 +0530

    Update trending songs for April 2026

---
+++ lib/trending-songs.ts
@@ -0,0 +1,7 @@
+export const TRENDING_SONGS = [
+    { title: "Tu Meri Main Tera", artist: "Pritam & Arijit Singh", hits: "25.8M" },
+    { title: "Gehra Hua (Dhurandhar)", artist: "Arijit Singh", hits: "21.4M" },
+    { title: "Sitaare (Ikkis)", artist: "Arijit Singh", hits: "18.2M" },
+    { title: "Usey Kehna (Tere Ishk Mein)", artist: "A.R. Rahman & Jonita Gandhi", hits: "15.9M" },
+    { title: "Chillgum", artist: "Yo Yo Honey Singh & Malaika Arora", hits: "14.5M" },
+];
